bdd39d4ec2c646e512798992753b3877e288c9ed
akshayTrustG <akshayk@trustgrid.com>
2024-05-17 12:25:19 +0530

    node repo setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('testing...');
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
