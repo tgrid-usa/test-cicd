8782fc5b417f36eb388308ccb06f9d105cc97bf8
AC_Atul_Trust_G <167758389+atultg@users.noreply.github.com>
2024-05-22 12:07:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, HBHK');
+  res.send('Hello, HBHKSNDKJ');
 });
 
 app.get('/test', (req, res) => {
