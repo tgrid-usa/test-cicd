24817e39f4337cfc7618836f9705117a96da79a3
devopstrustgrid <devops@trustgrid.com>
2025-02-18 15:43:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, TGJ');
+  res.send('This latest Hello, TGJ');
 });
 
 app.get('/test', (req, res) => {
